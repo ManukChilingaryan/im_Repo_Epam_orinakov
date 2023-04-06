e7a22951b5cf86b8fcd204efdc1a24a4f7fb3765
Albert Hovhannisyan <alberthovhannisyan.tt118-1@polytechnic.am>
2023-04-07 00:28:43 +0400

    homework done

---
+++ JS_Core/hometask_2_variables_data_types/data_type_practice.js
@@ -0,0 +1,9 @@
+const number = 7;
+console.log(typeof number);
+
+const convertPrev = number.toString();
+console.log(typeof convertPrev);
+
+const reConvert = Number(convertPrev);
+console.log(typeof reConvert);
+
---
+++ JS_Core/hometask_2_variables_data_types/variables_practice.js
@@ -0,0 +1,9 @@
+let someName = 'Albert';
+console.log(someName);
+
+someName = 'Hovhannisyan';
+let favoriteNumber = 7;
+let divided = favoriteNumber / 2;
+let michaelsFavorite = 13;
+michaelsFavorite = 7;
+console.log(divided - michaelsFavorite);
