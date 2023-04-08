acf18af497cd24c33915570e52524b22806484ec
vsergoyan <vardemi.sergoyan1@gmail.com>
2023-04-08 23:57:21 +0400

    All tasks are done for hometask2!

---
+++ JS_Core/hometask_2_variables_data_types/vardeni-sergoyan/begginer_practice.js
@@ -0,0 +1,27 @@
+//1
+let carName;
+carName = "Volvo";
+
+ //2
+let years;
+years = 50;
+
+//3
+let x, y;
+x = 5;
+y = 10;
+console.log(x + y);
+
+//4
+let z;
+z = x + y;
+console.log(z);
+
+//5
+let firstName = "John", lastName = "Doe", age  = 35;
+
+//6
+let a, b;
+a = 10;
+b = 5;
+console.log(a * b);
