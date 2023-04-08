4a1f81202e2087704420e52789b9bf84e90d099c
Hambarcum Khachatrian <hambarcum.khachatrian@gmail.com>
2023-04-08 15:13:38 +0400

    hometask_2 done

---
+++ JS_Core/hometask_2_variables_data_types/Hambarcum-Khachatrian/HambarcumKhachatrian.js
@@ -0,0 +1,89 @@
+//Beginner practice
+//1 
+let carName = 'Volvo';
+//2
+let years = 50;
+//3
+let x = 5, y = 10;
+console.log(x + y)
+//4
+let z = x + y;
+console.log(z)
+//5
+let firstName = "John", lastName = "Doe", age = 35;
+//6
+let ten = 10, five = 5;
+console.log(ten * five)
+
+//The Fortune Teller
+//1 
+let childrenNum = 1;
+let partnersName = 'Tamara';
+let location = 'Gyumri';
+let job = 'unemployed';
+//2 
+let string = `You will be a ${job} in ${location}, and married to ${partnersName} with ${childrenNum} kids.`
+console.log(string)
+/*
+console.log(`You will be a ${job} in ${location}, and married to ${partnersName} with ${childrenNum} kids.`)
+*/
+
+
+//Data type practice
+
+//1 
+let myNum = 46;
+console.log(typeof (myNum));
+//2 
+let myNumString = myNum.toString();
+console.log(typeof (myNumString));
+//3 
+let myNumNumber = Number(myNumString);
+// = myNumString*1, +myNumString
+console.log(typeof (myNumNumber));
+
+//Variables Practice
+
+//1 
+let someName = 'Hambarcum';
+//2
+console.log(someName);
+//3 
+someName = 'Khachatrian';
+//4 
+let favoriteNumber = myNum;
+//5 
+console.log(favoriteNumber / 2);
+//6 
+let michaelsFavorite = 13;
+//7
+michaelsFavorite = 7;
+//8 
+michaelsFavorite = michaelsFavorite - favoriteNumber;
+console.log(michaelsFavorite)
+
+// Type prediction
+/*
+1 number
+2 number
+3 number
+4 string
+5 boolean
+6 boolean
+7 bigint
+
+*/
+
+//  Type conversions
+
+/*
+1  123  number
+2  1  number
+3  0  number
+4  true  boolean
+5  false  boolean
+6  false  boolean
+7  true  boolean
+8  infinity  number
+9  NaN  number
+*/
