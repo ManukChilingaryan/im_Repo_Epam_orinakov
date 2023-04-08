78addc783b3120c50c3abbfc8e937606278ceb27
mariam-kobelyan <kobelyan03@gmail.com>
2023-04-08 21:27:19 +0400

    Add hometask 2

---
+++ JS_Core/hometask_2_variables_data_types/mariam-kobelyan/dataTypePractice.js
@@ -0,0 +1,11 @@
+//1
+let favNum = 7
+console.log(typeof(favNum))
+
+//2
+let favNumStr = favNum.toString()
+console.log(typeof(favNumStr))
+
+//3
+let strToNum = Number(favNumStr)
+console.log(typeof(strToNum))
---
+++ JS_Core/hometask_2_variables_data_types/mariam-kobelyan/variablesPractice.js
@@ -0,0 +1,13 @@
+let someName = "Mariam"
+console.log(someName)
+someName = "Kobelyan"
+
+let favoriteNumber = 3
+
+favoriteNumber = favoriteNumber/2
+
+let michaelsFavorite = 13
+
+michaelsFavorite = 7
+
+console.log(michaelsFavorite - favoriteNumber)
