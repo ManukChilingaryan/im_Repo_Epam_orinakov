3e86ddc02f47bbfb96a7b7cf9ba6025ec6112202
Hambarcum Khachatrian <hambarcum.khachatrian@gmail.com>
2023-04-10 21:02:31 +0400

    correct errors

--- JS_Core/hometask_2_variables_data_types/Hambarcum-Khachatrian/HambarcumKhachatrian.js
+++ JS_Core/hometask_2_variables_data_types/Hambarcum-Khachatrian/HambarcumKhachatrian.js
@@ -1,44 +1,45 @@
 //Beginner practice
 //1 
-let carName = 'Volvo';
+const carName = 'Volvo';
 //2
-let years = 50;
+const years = 50;
 //3
-let x = 5, y = 10;
-console.log(x + y)
+const x = 5, y = 10;
+console.log(x + y);
 //4
-let z = x + y;
-console.log(z)
+const z = x + y;
+console.log(z);
 //5
-let firstName = "John", lastName = "Doe", age = 35;
+const firstName = "John", lastName = "Doe", age = 35;
 //6
-let ten = 10, five = 5;
-console.log(ten * five)
+const ten = 10, five = 5;
+console.log(ten * five);
 
 //The Fortune Teller
 //1 
-let childrenNum = 1;
-let partnersName = 'Tamara';
-let location = 'Gyumri';
-let job = 'unemployed';
+const childrenNum = 1;
+const partnersName = 'Tamara';
+const location = 'Gyumri';
+const job = 'unemployed';
 //2 
-let string = `You will be a ${job} in ${location}, and married to ${partnersName} with ${childrenNum} kids.`
-console.log(string)
-/*
-console.log(`You will be a ${job} in ${location}, and married to ${partnersName} with ${childrenNum} kids.`)
-*/
+
+console.log(`You will be a ${job} in ${location}, and married to ${partnersName} with ${childrenNum} kids.`);
+
+// const string = `You will be a ${job} in ${location}, and married to ${partnersName} with ${childrenNum} kids.`
+// console.log(string)
+
 
 
 //Data type practice
 
 //1 
-let myNum = 46;
+const myNum = 46;
 console.log(typeof (myNum));
 //2 
-let myNumString = myNum.toString();
+const myNumString = myNum.toString();
 console.log(typeof (myNumString));
 //3 
-let myNumNumber = Number(myNumString);
+const myNumNumber = Number(myNumString);
 // = myNumString*1, +myNumString
 console.log(typeof (myNumNumber));
 
@@ -51,16 +52,16 @@ console.log(someName);
 //3 
 someName = 'Khachatrian';
 //4 
-let favoriteNumber = myNum;
+const favoriteNumber = myNum;
 //5 
 console.log(favoriteNumber / 2);
 //6 
-let michaelsFavorite = 13;
+const michaelsFavorite = 13;
 //7
 michaelsFavorite = 7;
 //8 
 michaelsFavorite = michaelsFavorite - favoriteNumber;
-console.log(michaelsFavorite)
+console.log(michaelsFavorite);
 
 // Type prediction
 /*
