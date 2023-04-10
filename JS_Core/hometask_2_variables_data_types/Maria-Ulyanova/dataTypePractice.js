4bfb541c8a717c0e1a1852d2bfff9a374db74fcf
Maria Ulyanova <ulyanovamari1@gmail.com>
2023-04-10 12:21:30 +0400

    change file dataTypePractice

--- JS_Core/hometask_2_variables_data_types/Maria-Ulyanova/dataTypePractice.js
+++ JS_Core/hometask_2_variables_data_types/Maria-Ulyanova/dataTypePractice.js
@@ -1,8 +1,8 @@
 const myFavoriteNumber = 8;
 console.log(typeof myFavoriteNumber); //number
 
-const strConver = toString(myFavoriteNumber);
+const strConver = String(myFavoriteNumber);
 console.log(typeof strConver); //string
 
-const backToNumber = +strConver;
+const backToNumber = Number(strConver);
 console.log(typeof backToNumber); //number
