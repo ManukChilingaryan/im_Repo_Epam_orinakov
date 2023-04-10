a73580d51df344e5a96748904dcee6e1a2139b0c
unknown <stella.evinyan.y@tumo.org>
2023-04-10 15:31:33 -0700

    added solutions for two tasks

---
+++ JS_Core/hometask_2_variables_data_types/Stella-Hw.js
@@ -0,0 +1,18 @@
+let carName = "Volvo";
+let years = 50;
+let x = 5;
+let y = 10;
+console.log(x+y);
+let z = x + y;
+console.log(z);
+let firstName = "John"; const lastName = "Doe"; const age = 35;
+let a = 10;
+let b = 5;
+console.log(a*b);
+
+let N = 4; //number of children
+let P = "Stella"; //partner's name
+let X = "teacher"; //job title
+let Y = "Yerevan"; //geographic location
+
+console.log(`You will be a ${X} in ${Y}, and married to ${P} with ${N} kids.`);  
