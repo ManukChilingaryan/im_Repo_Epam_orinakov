cca11f31faa177969e24376e771285c63aafb14e
vsergoyan <vardemi.sergoyan1@gmail.com>
2023-04-11 07:47:20 +0400

    Fixed all files

---
+++ JS_Core/hometask_2_variables_data_types/vardeni-sergoyan/dataTypePractice.js
@@ -0,0 +1,11 @@
+//1
+const n = 7;
+console.log(typeof(n));
+
+//2
+const str = String(n);
+console.log(typeof(str));
+
+//3
+const n1 = Number(str);
+console.log(typeof(n1));
---
+++ JS_Core/hometask_2_variables_data_types/vardeni-sergoyan/theFortuneTeller.js
@@ -0,0 +1,5 @@
+//1
+const n = 3, husband = "John", loc = "Epam", job = "programmer";
+
+//2
+console.log(`You will be a ${job} in ${loc}, and married to ${husband} with ${n} kids.`);
