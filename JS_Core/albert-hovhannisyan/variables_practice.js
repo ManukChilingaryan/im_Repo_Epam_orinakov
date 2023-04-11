f6fdeb98dbdb956a5aac4594e83fccb4fdd329ab
Albert Hovhannisyan <alberthovhannisyan.tt118-1@polytechnic.am>
2023-04-11 17:22:22 +0400

    bug fixed

--- JS_Core/albert-hovhannisyan/variables_practice.js
+++
@@ -1,9 +0,0 @@
-let someName = 'Albert';
-console.log(someName);
-
-someName = 'Hovhannisyan';
-let favoriteNumber = 7;
-let divided = favoriteNumber / 2;
-let michaelsFavorite = 13;
-michaelsFavorite = 7;
-console.log(divided - michaelsFavorite);
---
+++ JS_Core/albert-hovhannisyan/variables_pratice.js
@@ -0,0 +1,12 @@
+const someName = 'Albert';
+console.log(someName);
+
+someName = 'Hovhannisyan';
+
+const favoriteNumber = 7;
+const divided = favoriteNumber / 2;
+
+const michaelsFavorite  = 13;
+michaelsFavorite  = 7;
+
+console.olog(michaelsFavorite - divided);
