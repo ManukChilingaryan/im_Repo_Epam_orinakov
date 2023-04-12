e8b216a98e838d6004b33722feb5898295d6ec7d
Manuk1991 <chilingaryanmanuk@gmail.com>
2023-04-12 23:37:49 +0300

    code for solution

---
+++ JS_Core/hometask_2_variables_data_types/ManukChilingaryan/Variables_Practice.js
@@ -0,0 +1,39 @@
+
+//1.Set a variable called someName equal to your name. Put your name in quotation marks.
+
+let someName = "Manuk";
+//2.Display the value of someName in the console.
+
+console.log(someName); // Manuk
+
+//3.Change the value of someName to your surname.
+
+someName = "Chilingaryan"; 
+
+//4.Set a variable called favoriteNumber equal to your favorite number.
+
+const favoriteNumber = 25;
+
+//5.Calculate what your favorite number divided by 2 is.
+
+let fav_Num_divided_2 = favoriteNumber / 2;
+
+console.log(fav_Num_divided_2 );  // 12.5
+
+
+//6.Set another variable called michaelsFavorite equal to 13.
+
+let michaelsFavorite = 13;
+
+
+//7.Change the value of michaelsFavorite to 7.
+
+michaelsFavorite = 7;
+
+//8.Subtract your favorite number after devision from the latest michaelsFavorite number.
+
+console.log(michaelsFavorite - favoriteNumber); // -18
+
+
+
+
