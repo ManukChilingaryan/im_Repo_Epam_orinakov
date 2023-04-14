02a5b7689e11dd743e9abc3d477bc05229986b35
Maria Ulyanova <ulyanovamari1@gmail.com>
2023-04-14 18:44:29 +0400

    task2 fixed

--- JS_Core/hometask_3_operators_conditions_loops/Maria-Ulyanova/task2.js
+++ JS_Core/hometask_3_operators_conditions_loops/Maria-Ulyanova/task2.js
@@ -1,29 +1,18 @@
-const word1 = "Algorism"; //The word is Isogram
-const word2 = "Password"; //The word isn’t isogram
-const word3 = "PasSword"; //The word isn’t isogram
-const word4 = "Unforgiveable"; //The word isn’t isogram
-const word5 = "Moose"; //The word isn’t isogram
-
-const words = [word1, word2, word3, word4, word5];
+const words = ["Algorism", "Password", "PasSword", "Unforgiveable", "Moose"];
 
 // Loop through each word
 for (let i = 0; i < words.length; i++) {
-  let word = words[i];
   let isRepeated = false;
 
   // Loop through each character in the word
-  for (let j = 0; j < word.length; j++) {
-    let char = word[j].toLowerCase();
-     // Check if the character appears again in the word
-    switch (word.slice(j + 1).toLowerCase().indexOf(char)) {
-      case -1:
+  for (let j = 0; j < words[i].length; j++) {
+     
+    if (words[i].slice(j + 1).toLowerCase().indexOf(words[i][j].toLowerCase()) === -1) {
         // If the character doesn't appear again, continue to the next character
-        break;
-      default:
-         // If the character appears again, the word is not an isogram
-        console.log("The word isn’t isogram");
-        isRepeated = true;
-        break;
+    } else {
+      // If the character appears again, the word is not an isogram
+      console.log("The word isn’t isogram");
+      isRepeated = true;
     }
     // If the word is not an isogram, break out of the character loop
     if (isRepeated) {
@@ -34,4 +23,4 @@ for (let i = 0; i < words.length; i++) {
   if (!isRepeated) {
     console.log("The word is Isogram");
   }
-}
+};
