89daae6286622e7003fe9cc489ed03debcca3432
Maria Ulyanova <ulyanovamari1@gmail.com>
2023-04-17 16:00:20 +0400

    add requested changes

--- JS_Core/hometask_4_functions/Maria-Ulyanova/bonusTask.js
+++ JS_Core/hometask_4_functions/Maria-Ulyanova/bonusTask.js
@@ -3,15 +3,15 @@
 // Requirements:
 // Have the function StringChallenge(str) read str which will contain two strings separated by a space.
 // The first string will consist of the following sets of characters: +, *, and (N) which is optional.
-// The plus (+) character represents a single alphabetic character, the asterisk (*) represents a sequence of the same 
-// character of length 3 unless it is followed by {N} which represents how many characters should appear in the sequence where N 
-// will be at least 1.
+// The plus (+) character represents a single alphabetic character, the asterisk (*) represents a sequence 
+// of the same character of length 3 unless it is followed by {N} which represents how many characters should appear 
+// in the sequence where N will be at least 1.
 // Your goal is to determine if the second string exactly matches or not the pattern of the first string in the input.
 // FOR EXAMPLE
 
 // If str is “+++++* abcdemmmmmm” then the second string in this case matches the pattern, so your program should return the string false.
 
-// If str is “**+*{5} mmmrrrkbb” then the second string does not match the pattern, your program should return the string true.
+// If str is “**+*{2} mmmrrrkbb” then the second string does not match the pattern, your program should return the string true.
 
 function StringChallenge(str) {
   let [str1, str2] = str.split(' ');
@@ -20,7 +20,7 @@ function StringChallenge(str) {
 
   while (i < str1.length) {
     if (str1[i] === '+') {
-      if (!symbols.test(str2[0])) {   
+      if (str2[0] === undefined || !symbols.test(str2[0])) {   
         return false;
       }
       str2 = str2.slice(1);  
@@ -28,7 +28,7 @@ function StringChallenge(str) {
     else if (str1[i] === '*') {
       let j = 1, k = 0;
       if (str1[i + 1] !== undefined && str1[i + 1] ==='{') {
-        k = parseInt(str1[i +2 ]);
+        k = parseInt(str1[i +2]);
         i = i + 4;
       }
        else {
@@ -37,7 +37,7 @@ function StringChallenge(str) {
        }
 
        while (j <= k) {
-        if (!symbols.test(str2[0])) {  
+        if (str2[0] === undefined || !symbols.test(str2[0])) {  
           return false;
         }
         str2 = str2.slice(1); 
@@ -55,4 +55,4 @@ function StringChallenge(str) {
 }
 
 console.log(StringChallenge("+++++* abcdemmmmmm")); //false
-console.log(StringChallenge("**+*{5} mmmrrrkbb")); //true
+console.log(StringChallenge("**+*{2} mmmrrrkbb")); //true
